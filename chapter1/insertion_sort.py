baa8c1bcc95a5db610de80536a3ec1a4f98b4bc6
Joydeep Bhattacharjee <joydeepubuntu@gmail.com>
2016-07-07 16:23:04 +0530

    modified insertion_sort to workable
    the module is working now

--- chapter1/insertion_sort.py
+++ chapter1/insertion_sort.py
@@ -1,3 +1,9 @@
+from random import randint
+
+try:
+    range = xrange
+except:
+    pass
 
 def insertion_sort(items_list):
     """
@@ -10,7 +16,11 @@ def insertion_sort(items_list):
         while j>0 and (items_list[j] < items_list[j-1]):
             items_list[j], items_list[j-1] = items_list[j-1], items_list[j]
             j -= 1
+    return items_list
+
 
 if __name__ == "__main__":
-    sorted_ans = insertion_sort([3,2,1])
-    print(sorted_ans)
+    unsorted_list = [randint(0,9) for p in range(0,9)]
+    print unsorted_list
+    sorted_ans = insertion_sort(unsorted_list)
+    print sorted_ans
